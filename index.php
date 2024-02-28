209aa314669b5e9b8b6e7ec32cf5ba281a9d3c61
hosein rezaei <hosein.rezaei72@gmail.com>
2024-02-28 11:16:00 +0330

    show vehicle list

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
 require 'vendor/autoload.php';
 
-$inpute = cli\input();
-$msg = $inpute ;
-cli\out($msg);
+$vehiclesData = file_get_contents('resource/vehicles.json');
+$vehicles = json_decode($vehiclesData, true);
+
+// Display vehicles list
+echo "Vehicles list:\n";
+$i = 1;
+foreach ($vehicles as $vehicle) {
+    cli\out("#$i) {$vehicle['name']} (Max Speed: {$vehicle['maxSpeed']} {$vehicle['unit']})\n");
+    $i++;
+}
+
+cli\out("\nPlayer 1, choose your vehicle number: ");
+$playerOneChoose = cli\input();
+
+cli\out("\nPlayer 2, choose your vehicle number: ");
+$playerTwoChoose = cli\input();
+
