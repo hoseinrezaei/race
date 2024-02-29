bd7105f04ae9199a4ec6ee4da96536729b9f350a
hosein rezaei <hosein.rezaei72@gmail.com>
2024-02-29 23:56:16 +0330

    add setter and getter methods in vehicle model

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -5,5 +5,29 @@ namespace App\Models;
 
 class Vehicle
 {
+    private string $name;
+    private int $maxSpeed;
+    private string $unit;
 
+    public function __construct($name, $maxSpeed, $unit)
+    {
+        $this->name = $name;
+        $this->maxSpeed = $maxSpeed;
+        $this->unit = $unit;
+    }
+
+    public function getName() :string
+    {
+        return $this->name;
+    }
+
+    public function getMaxSpeed() :int
+    {
+        return $this->maxSpeed;
+    }
+
+    public function getUnit() :string
+    {
+        return $this->unit;
+    }
 }
